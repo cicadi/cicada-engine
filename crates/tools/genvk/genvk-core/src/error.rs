9ca98ceeb82ba5caed6e8c7b070ae73f6b52903a
cicadi <cicadi.dev@gmail.com>
2025-04-03 23:00:50 +0330

    Tweaked the Error type in crate `genvk-core`.

--- crates/tools/genvk/genvk-core/src/error.rs
+++ crates/tools/genvk/genvk-core/src/error.rs
@@ -1,6 +1,10 @@
+use crate::parse;
+
 #[derive(Debug)]
 pub enum Error {
-    Parse(genvk_parse::error::Error),
+    Io(std::io::Error),
+
+    Parse(crate::parse::Error),
 }
 
 impl std::error::Error for Error {}
@@ -8,13 +12,21 @@ impl std::error::Error for Error {}
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            Error::Io(e) => e.fmt(f),
+
             Error::Parse(e) => e.fmt(f),
         }
     }
 }
 
-impl From<genvk_parse::error::Error> for Error {
-    fn from(value: genvk_parse::error::Error) -> Self {
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Self::Io(value)
+    }
+}
+
+impl From<crate::parse::Error> for Error {
+    fn from(value: parse::Error) -> Self {
         Self::Parse(value)
     }
 }
--- crates/tools/genvk/genvk-core/src/parse.rs
+++ crates/tools/genvk/genvk-core/src/parse.rs
@@ -1,1 +1,1 @@
-pub use genvk_parse::{registry::*, *};
+pub use genvk_parse::{error::*, registry::*, *};
